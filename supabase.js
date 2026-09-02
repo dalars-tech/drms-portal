81f1cf83a5b23422473f5d28fa0db2bd312e64a4
dalars-tech <bert36766@gmail.com>
2026-09-02 20:46:43 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://bpedjgnixeccxchmykhr.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://bpedjgnixeccxchmykhr.supabase.co";
 const SUPABASE_KEY = "sb_publishable_oESVmVNM5MrDSMqTiULYWQ_4Wzmd0YN";
 
 const supabaseClient = window.supabase.createClient(
